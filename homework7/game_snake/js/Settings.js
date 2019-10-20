3163e4571cfbc0bb49237ccf1ae37e46a42220e8
vladovinkin <vladovinkin@gmail.com>
2019-10-20 09:08:18 +0300

    функция инициализации настроек

---
+++ homework7/game_snake/js/Settings.js
@@ -0,0 +1,37 @@
+'use strict';
+
+class Settings {
+	/**
+	/* @param {Object} params - Параметры игры.
+	/* @param {number} params.rowsCount - количество строк игрового поля
+	/* @param {number} params.colsCount - количество колонок игрового поля
+	/* @param {number} params.speed - скорость перемещения змейки
+	/* @param {number} params.winLength - длина змейки для окончания уровня
+	/* @throw {Error} если переданы не верные настройки, выбрасывается
+	/* соответствующая ошибка.
+	*/
+	init(params) {
+		let defaultParams = {rowsCount: 21, colsCount: 21, speed: 2, winLength: 50};
+		Object.assign(defaultParams, params);
+		
+		if (defaultParams.rowsCount < 10 || defaultParams.rowsCount > 30) {
+			throw new Error('Неверные настройки, значение RowsCount должно быть в диапазоне [10, 30].');
+		}
+		this.rowsCount = defaultParams.rowsCount;
+		
+		if (defaultParams.colsCount < 10 || defaultParams.colsCount > 30) {
+			throw new Error('Неверные настройки, значение colsCount должно быть в диапазоне [10, 30].');
+		}
+		this.colsCount = defaultParams.colsCount;
+		
+		if (defaultParams.speed < 1 || defaultParams.speed > 10) {
+			throw new Error('Неверные настройки, значение speed должно быть в диапазоне [1, 10].');
+		}
+		this.speed = defaultParams.speed;
+		
+		if (defaultParams.winLength < 5 || defaultParams.winLength > 50) {
+			throw new Error('Неверные настройки, значение winLength должно быть в диапазоне [5, 50].');
+		}
+		this.winLength = defaultParams.winLength;
+	}
+}
